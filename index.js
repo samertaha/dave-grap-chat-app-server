86826919405c0f7108a85d9e04d4f4eb3a01d52c
Samer taha <alrazy1@gmail.com>
2023-11-04 13:44:38 +0200

    good

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const ws = require('ws');
 const server = new ws.Server({ port: '3000' });
 
-server.on('connection', socket);
-console.log('hi');
+server.on('connection', socket => {
+    socket.on('message', message => {
+        const b = Buffer.from(message);
+        console.log(b.toString('utf-8'));
+        socket.send(`${message}`);
+    });
+});
