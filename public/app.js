5309175ecb56d1f26558ca7a4f622842271f97de
Samer taha <alrazy1@gmail.com>
2023-11-05 17:10:12 +0200

    completed front end app.js functions

--- public/app.js
+++ public/app.js
@@ -45,9 +45,26 @@ msgInput.addEventListener('keypress', () => {
 //Listen for messages
 socket.on('message', (data) => {
     activity.textContent = '';
+    const { name, text, time } = data;
     const li = document.createElement('li');
-    li.textContent = data;
-    document.querySelector('ul').appendChild(li);
+    li.className = 'post';
+    if (name === nameInput.value) li.className = 'post post--left';
+    if (name !== nameInput.value && name !== ADMIN) li.className = 'post post--right';
+    if (name !== 'ADMIN') {
+        li.innerHTML = `<div class="post__header ${name === nameInput.value
+            ? 'post__header--user'
+            : 'post__header--reply'
+            }" >
+            <span class="post__header--name">${name}</span>
+            <span class="post__header--time">${time}</span>
+            </div>
+            <div class="post__text">${text}</div>`;
+    } else {
+        li.innerHTML = `<div class="post__text">${text}</div>`;
+    }
+    document.querySelector('.chat-display').appendChild(li);
+
+    chatDisplay.scrollTop = chatDisplay.scrollHeight;
 });
 
 let activityTimer;
@@ -60,3 +77,32 @@ socket.on("activity", (name) => {
         activity.textContent = '';
     }, 3000);
 });
+
+socket.on('userList', ({ users }) => {
+    showUsers(users);
+});
+
+socket.on('roomList', ({ rooms }) => {
+    showRooms(rooms);
+});
+
+function showUsers(users) {
+    usersList.textContent = '';
+    if (users) {
+        usersList.innerHTML = `<em>Users in ${chatRoom.value} chat room</em>`;
+        users.forEach((user, i) => {
+            usersList.textContent += ` $(user.name)`;
+            if (users.length > 1 && i !== users.length - 1) usersList.textContent += ', ';
+        });
+    }
+}
+function showRooms(rooms) {
+    roomList.textContent = '';
+    if (rooms) {
+        roomList.innerHTML = '<em>Active Rooms:</em>';
+        rooms.forEach((room, i) => {
+            roomList.textContent += ` ${room}`;
+            if (rooms.length > 1 && i !== rooms.length - 1) roomList.textContent += ',';
+        });
+    }
+}
